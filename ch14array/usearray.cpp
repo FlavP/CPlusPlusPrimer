65ef454873d86a6bd5ac38fd929206e5c731b22e
flavius <flavius.popoiu@mediaimage.ro>
2017-10-09 14:23:51 +0300

    chapter 14 array with main

--- ch14array/usearray.cpp
+++ ch14array/usearray.cpp
@@ -24,7 +24,26 @@ int main(void){
     using std::endl;
     ArrayTP<int, 10> sums;
     ArrayTP<double, 10> averages;
+    ArrayTP<ArrayTP<int, 5>, 10> combined;
+    int i, j;
+    for(i = 0; i < 10; i++){
+        sums[i] = 0;
+        for(j = 0; j < 5; j++){
+            combined[i][j] = (i + 1) * (j + 1);
+            sums[i] += combined[i][j];
+        }
+        averages[i] = (double) sums[i] / 10;
+    }
     
+    for(i = 0; i < 10; i++){
+        for(j = 0; j < 5; j++){
+            cout.width(2);
+            cout << combined[i][j] << ' ';
+        }
+        cout << ": sum: " << endl;
+        cout.width(3);
+        cout << sums[i] << " averages: " << averages[i] << endl;
+    }
     return 0;
 }
 
