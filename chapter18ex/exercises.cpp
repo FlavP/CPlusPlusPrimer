34e89bd19a28a37674e129b58979526b4b7b2c75
flavius <flavius.popoiu@mediaimage.ro>
2017-11-06 13:59:57 +0200

    exercises

--- chapter18ex/exercises.cpp
+++ chapter18ex/exercises.cpp
@@ -11,7 +11,9 @@
  * Created on November 6, 2017, 11:52 AM
  */
 
+#include <iostream>
 #include <cstdlib>
+#include <array>
 
 using namespace std;
 
@@ -34,11 +36,45 @@ std::string s {"What a bracing effect!"};
 int k {99};
 Z200 zip{200, 'Z', 0.67};
 std::vector<int> ai {3, 9, 4, 7, 1};
-*/
 
+template<class T>
+void show2(double x, T fp) {cout << x << " -> " << fp(x) << '\n';}
+double f1(double x) {return 1.8*x + 32;}
 
 int main() {
+    //show2(18.0, f1);
+    show2(18.0, [](double x) {return 1.8 * x + 32;});
+    return 0;
+}
+*/
 
+const int Size = 5; 
+template<typename T>
+
+void sum(array<double, Size> a, T fp);
+
+class Adder{
+    double tot;
+public:
+    Adder(double q = 0) : tot(q){}
+    void operator()(double w) {tot += w;}
+    double tot_v() const {return tot;};
+};
+
+int main(){
+    double total = 0.0;
+    Adder ad(total);
+    array<double, Size> temp_c = {32.1, 34.3, 37.8, 35.2, 34.7};
+    //sum(temp_c, ad);
+    sum(temp_c, [&total](double x){total += x;});
+    //total = ad.tot_v();
+    cout << "total: " << total << '\n';
     return 0;
 }
 
+template<typename T>
+void sum(array<double, Size> a, T fp){
+    for(auto pt = a.begin(); pt != a.end(); pt++){
+        fp(*pt);
+    }
+}
