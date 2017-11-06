b2d4dda8e76c6a85dd30047b4476abdb1cc94da7
flavius <flavius.popoiu@mediaimage.ro>
2017-11-06 12:30:56 +0200

    exercises

---
+++ chapter18ex/exercises.cpp
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   exercises.cpp
+ * Author: flavius
+ *
+ * Created on November 6, 2017, 11:52 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ 
+
+Review exercise one, with braces
+class Z200{
+private:
+    int j;
+    char ch;
+    double z;
+public:
+    Z200(int jv, char chv, zv) : j(jv), ch(chv), z(zv){}
+};
+
+double x {8.8}; // or = {8.8}
+std::string s {"What a bracing effect!"};
+int k {99};
+Z200 zip{200, 'Z', 0.67};
+std::vector<int> ai {3, 9, 4, 7, 1};
+*/
+
+
+int main() {
+
+    return 0;
+}
+
